2d991c6a55e7f329ab6cf2e26e90541892b2ba9b
idasegolsson <seid24yr@student.ju.se>
2025-04-24 21:37:51 +0200

    added dropdown menu

--- navigationbar.js
+++ navigationbar.js
@@ -5,3 +5,18 @@
 // function closeCategoryMenu() {
 //   document.getElementById("categoryMenu").style.display = "none";
 // }
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
