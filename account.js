5d477e8a80dfba2b5c60005735fae6b37604d72d
pehe24kv <pehe24kv@student.ju.se>
2025-05-09 14:33:12 +0200

    Deleted outcommented code

--- account.js
+++ account.js
@@ -1,40 +1,3 @@
-// const reviews = JSON.parse(localStorage.getItem("reviews")) || [];
-// const display = document.getElementById("review-display");
-
-// if (reviews.length === 0) {
-//   display.innerHTML =
-//     "<p>There are no reviews yet. Add one to your collection.</p>";
-// } else {
-//   reviews.forEach((r) => {
-//     const reviewBox = document.createElement("div");
-//     reviewBox.classList.add("review-box");
-
-//     const title = document.createElement("h3");
-//     title.textContent = r.title;
-
-//     const stars = document.createElement("div");
-//     stars.classList.add("stars");
-
-//     for (let i = 0; i < r.stars; i++) {
-//       stars.innerHTML += '<i class="fa-solid fa-star clicked"></i>';
-//     }
-//     for (let i = r.stars; i < 5; i++) {
-//       stars.innerHTML += '<i class="fa-solid fa-star"></i>';
-//     }
-
-//     const text = document.createElement("p");
-//     text.textContent = r.text;
-
-//     // Lägg till allt i reviewBox
-//     reviewBox.appendChild(title);
-//     reviewBox.appendChild(stars);
-//     reviewBox.appendChild(text);
-
-//     // Lägg till reviewBox i display-elementet
-//     display.appendChild(reviewBox);
-//   });
-// }
-
 const reviews = JSON.parse(localStorage.getItem("reviews")) || [];
 const display = document.getElementById("review-display");
 
@@ -55,6 +18,7 @@ if (reviews.length === 0) {
           <h3>${r.title}</h3>
           <div class="stars">${filledStars}${emptyStars}</div>
           <p>${r.text}</p>
+          <i class="fa-solid fa-trash-can"></i>
         </div>
       `;
     })
