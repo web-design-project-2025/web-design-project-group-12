b5a8151e3045639f0a5ba4f340a28cb231e485eb
pehe24kv <pehe24kv@student.ju.se>
2025-05-10 11:39:48 +0200

    Tried to make a trashcan for reviews work - will keep on working on the feature during next labsession

--- account.js
+++ account.js
@@ -14,17 +14,30 @@ if (reviews.length === 0) {
       const emptyStars = '<i class="fa-solid fa-star"></i>'.repeat(5 - r.stars);
 
       return `
-        <div class="review-box">
+        <div class="review-box" data-id="${r.id}">
           <h3>${r.title}</h3>
           <div class="stars">${filledStars}${emptyStars}</div>
           <p>${r.text}</p>
-          <i class="fa-solid fa-trash-can"></i>
+          <i class="fa-solid fa-trash-can" data-id="${r.id}"></i>
         </div>
       `;
     })
     .join("");
 }
 
+const trashIcons = document.querySelectorAll(".fa-trash-can");
+trashIcons.forEach((trashIcon) => {
+  trashIcon.addEventListener("click", function () {
+    for (let review of reviews) {
+      reviews.splice(reviews.indexOf(review), 1);
+      console.log(reviews.indexOf(review) + " " + review.title);
+      localStorage.setItem("reviews", JSON.stringify(reviews));
+      renderReviews();
+    }
+  });
+});
+
+// renderReviews();
 /*Snackbar - message for a posted review*/
 document.addEventListener("DOMContentLoaded", function () {
   if (localStorage.getItem("reviewAdded") === "true") {
